633ba56e0c20eb04799be99fe48b8be0fab1eee6
Ayman Sherief <aymanf.sherief@gmail.com>
2021-03-25 17:43:43 +0200

    Added base parser class

---
+++ parser/parser.py
@@ -0,0 +1,18 @@
+"""Parser mixin class for any paresr to implement"""
+from typing import Dict
+from typing import List
+
+
+class Parser:
+    """class for Parser interface"""
+
+    def __init__(self):
+        self._data: List[Dict] = []
+
+    def read(self, files: List[str]):
+        """stub for a parser method that reads in a list of files given their paths"""
+
+    @property
+    def data(self) -> List[Dict]:
+        """a computed instance attribute to protect data from changing by external code"""
+        return self._data
