70ef283a84544bf2967f9428bb890dd1670404ac
Ayman Sherief <aymanf.sherief@gmail.com>
2021-03-25 19:40:28 +0200

    Added csv parser and formatted xml parser

---
+++ parsers/csv_parser.py
@@ -0,0 +1,47 @@
+"""Code responsible for providing CSV parsing functionality"""
+import csv
+import logging
+import os
+from itertools import groupby
+from typing import Tuple
+
+from parsers.parser import Parser
+
+logger = logging.getLogger(__name__)
+
+
+class CSVParser(Parser):
+    """class That handles parsing CSV files"""
+
+    def read(self, files: Tuple[str, str]):
+        """
+        takes in a tuple [customers_csv_path, vehicles_csv_path] and parses it into python dicts,
+         sets the Parser data field
+
+        :param Tuple[str] files: a tuple of the two required files
+        :return: None, results are in the data property
+        """
+        for path in files:
+            if not os.path.exists(path):
+                raise ValueError(f"Couldn't find csv file {path}")
+
+        with open(files[0], encoding='utf-8') as customers_csv:
+            customers = list(csv.DictReader(customers_csv))
+            for i, c in enumerate(customers):
+                transaction = {'date': c.pop('date'), 'customer': c}
+                new_c = {'file_name': files[0], 'transaction': transaction}
+                customers[i] = new_c
+        with open(files[1], encoding='utf-8') as vehicles_csv:
+            vehicles = list(csv.DictReader(vehicles_csv))
+
+        customers = sorted(customers, key=lambda c: (c['transaction']['customer']['id']))
+        vehicles = sorted(vehicles, key=lambda v: (v['owner_id']))
+        for owner_index, (owner_id, vehicle_group) in enumerate(groupby(vehicles, key=lambda v: v['owner_id'])):
+            customer_vehicles = []
+            for v in vehicle_group:
+                del v['owner_id']
+                customer_vehicles.append(v)
+
+            customers[owner_index]['transaction']['vehicles'] = customer_vehicles
+
+        self._data = customers
---
+++ tests/test_parser/test_csv_parser.py
@@ -0,0 +1,74 @@
+import json
+import unittest
+from io import StringIO
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
+from parsers import XMLParser
+from parsers.csv_parser import CSVParser
+
+
+class TestXMLParser(unittest.TestCase):
+
+    @patch('os.path.exists')
+    @patch('parsers.csv_parser.open')
+    def test_read(self, mock_open: MagicMock, mock_exists: MagicMock):
+        mock_exists.return_value = True
+        mock_open.return_value.__enter__.side_effect = [StringIO("""
+"id","name","address","phone","date"
+"ID5410","Melissa T Miller","2837  Fidler Drive","210-624-7306","31/01/2020"
+"ID9857","Daniel I Walker","3853  Hilltop Street","413-655-7397","25/04/2020"
+""".strip()),
+StringIO(r"""
+"id","make","vin_number","owner_id"
+"V3015","Chevrolet","1HGFA16548L016469","ID9857"
+"V2014","Honda","1G6KD57Y46U180996","ID9857"
+"V1475","Ford","2HKYF18575H574967","ID5410"
+""".strip())]
+        parser = CSVParser()
+        parser.read(('csv/customers.csv', 'csv/vehicles.csv'))
+        expected_data = [{
+            "file_name": "csv/customers.csv",
+            "transaction": {
+                "date": "31/01/2020",
+                "customer": {
+                    "id": "ID5410",
+                    "name": "Melissa T Miller",
+                    "address": "2837  Fidler Drive",
+                    "phone": "210-624-7306"
+                },
+                "vehicles": [
+                    {
+                        "id": "V1475",
+                        "make": "Ford",
+                        "vin_number": "2HKYF18575H574967"
+                    }]
+            }
+        }, {
+            "file_name": "csv/customers.csv",
+            "transaction": {
+                "date": "25/04/2020",
+                "customer": {
+                    "id": "ID9857",
+                    "name": "Daniel I Walker",
+                    "address": "3853  Hilltop Street",
+                    "phone": "413-655-7397"
+                },
+                "vehicles": [
+                    {
+                        "id": "V3015",
+                        "make": "Chevrolet",
+                        "vin_number": "1HGFA16548L016469"
+                    },
+                    {
+                        "id": "V2014",
+                        "make": "Honda",
+                        "vin_number": "1G6KD57Y46U180996"
+                    }
+                ]
+            }
+        }
+        ]
+
+        actual_data = parser.data
+        self.assertEqual(json.dumps(expected_data), json.dumps(actual_data))
